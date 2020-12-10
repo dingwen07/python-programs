7c5f5cf4cf2a525683ea15c19760e8f0b7850c7d
王鼎文 <wangdingwen32@gmail.com>
2020-12-11 05:32:40 +0800

    Update RAM.py

--- calculus/RAM.py
+++ calculus/RAM.py
@@ -1,19 +1,28 @@
 from decimal import Decimal
 
 func = lambda x : Decimal('6') / x
-begin = Decimal('4')
+func = lambda x : Decimal('36') - x ** 2
+begin = Decimal('0')
 end = Decimal('6')
-nn = 100
-n = Decimal(str(nn))
-area = Decimal('0')
+num = 4
+
+i = Decimal(str(num))
+left_area = Decimal('0')
+mid_area = Decimal('0')
+right_area = Decimal('0')
 count = 0
 
 x = begin
-step = Decimal(str(( end - begin ) / n ))
+step = Decimal(str(( end - begin ) / i ))
 
-while count < nn:
+while count < num:
+    left_area = left_area + step * func(x)
+    mid_area = mid_area + step * func(x + step / 2)
     x = x + step
-    area = area + step * func(x)
+    right_area = right_area + step * func(x)
     count = count + 1
 
-print(area)
+print('Riemann Sum')
+print('Left: ' + str(left_area))
+print('Right: ' + str(right_area))
+print('Mid-point: ' + str(mid_area))
