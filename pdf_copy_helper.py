324330ff3a615eb2b5df312bd97ea3b3458f0ac1
Dingwen Wang <wangdingwen07@gmail.com>
2021-05-06 06:21:01 +0800

    Update pdf_copy_helper.py
    Catch exceptions

--- pdf_copy_helper.py
+++ pdf_copy_helper.py
@@ -8,15 +8,18 @@ def log(log_str: str):
 if __name__ == "__main__":
     old_clip = pyperclip.paste()
     while True:
-        time.sleep(1)
-        new_clip = pyperclip.paste()
-        if new_clip != old_clip:
-            text_list = new_clip.split(os.linesep)
-            text = ''
-            for i in range(0, len(text_list)):
-                text_list[i] = text_list[i].strip(' ')
-                text += text_list[i]
-                text += ' '
-            print('[PDF HELPER] {} lineseps removed'.format(str(len(text_list) - 1)))
-            pyperclip.copy(text.replace('\n', os.linesep))
-            old_clip = pyperclip.paste()
+        try: 
+            time.sleep(1)
+            new_clip = pyperclip.paste()
+            if new_clip != old_clip:
+                text_list = new_clip.split(os.linesep)
+                text = ''
+                for i in range(0, len(text_list)):
+                    text_list[i] = text_list[i].strip(' ')
+                    text += text_list[i]
+                    text += ' '
+                print('[PDF HELPER] {} lineseps removed'.format(str(len(text_list) - 1)))
+                pyperclip.copy(text.replace('\n', os.linesep))
+                old_clip = pyperclip.paste()
+        except Exception:
+            continue
